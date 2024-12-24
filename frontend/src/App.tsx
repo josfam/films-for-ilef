1895605b9c6a9aad15d0078ebab966f762bd90e6
josfam <jamukun@proton.me>
2024-12-25 00:25:42 +0300

    Add toast messages for already watched films

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,21 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FilmGrid from "./pages/FilmGrid";
 import MainLayout from "./Layouts/MainLayout";
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<FilmGrid />}></Route>
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      {/* Toast messages */}
+      <Toaster richColors position="top-center" visibleToasts={4} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<FilmGrid />}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- frontend/src/components/FilmCard.tsx
+++ frontend/src/components/FilmCard.tsx
@@ -1,3 +1,5 @@
+import { toast } from "sonner";
+
 interface FilmCardProps {
   coverName: string;
 }
@@ -12,7 +14,9 @@ const FilmCard = ({ coverName }: FilmCardProps) => {
           className="h-full w-full rounded-xl object-cover"
         />
         <div className="group absolute bottom-0 flex h-1/2 w-full scale-95 items-center justify-center rounded-bl-xl rounded-br-xl px-7 opacity-0 transition-all delay-75 duration-200 hover:scale-100 hover:opacity-100">
-          <button className="btn-pri">I've seen this one</button>
+          <button className="btn-pri"
+          onClick={() => toast.success("Okay, I will recommend another soon")}
+          >I've seen this one</button>
           {/* background overlay */}
           <div className="absolute h-full w-full rounded-bl-xl rounded-br-xl rounded-tl-3xl rounded-tr-3xl bg-gray-900 opacity-75"></div>
         </div>
