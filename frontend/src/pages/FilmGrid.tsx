3862bf9d8c23e3fd2cf1afdb48714e66c1de01b7
josfam <jamukun@proton.me>
2024-12-25 18:02:16 +0300

    Add unique key to each film card

--- frontend/src/pages/FilmGrid.tsx
+++ frontend/src/pages/FilmGrid.tsx
@@ -1,21 +1,29 @@
 import FilmCard from "../components/FilmCard";
 import filmList from "../utils/FilmList";
 import { filmInfo } from "../types/Types";
+import { v4 as uuidv4 } from "uuid";
 
-const FilmGrid = () => (
-  <div>
-    <div className="relative mb-16 flex h-fit flex-col items-center justify-center py-6">
-      <h1 className="absolute top-0 text-nowrap text-3xl font-semibold">
-        Films for Ilef
-      </h1>
-      <div className="absolute bottom-0 h-[2px] w-full rounded-sm bg-orange-400 bg-opacity-70"></div>
+const FilmGrid = () => {
+  return (
+    <div>
+      <div className="relative mb-16 flex h-fit flex-col items-center justify-center py-6">
+        <h1 className="absolute top-0 text-nowrap text-3xl font-semibold">
+          Films for Ilef
+        </h1>
+        <div className="absolute bottom-0 h-[2px] w-full rounded-sm bg-orange-400 bg-opacity-70"></div>
+      </div>
+      <div className="grid w-full grid-cols-1 gap-x-10 gap-y-14 text-sky-900 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {filmList.map((film: filmInfo) => (
+          <FilmCard
+            key={uuidv4()} // uuid unique key
+            coverName={`${film.coverName}`}
+            title={`${film.title}`}
+            released={`${film.released}`}
+          />
+        ))}
+      </div>
     </div>
-    <div className="grid w-full grid-cols-1 gap-x-10 gap-y-14 text-sky-900 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {filmList.map((film: filmInfo) => (
-        <FilmCard coverName={`${film.coverName}`} title={`${film.title}`} released={`${film.released}`}/>
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default FilmGrid;
