151d3219a1514d1de3d74492c27469dfa95b177e
josfam <jamukun@proton.me>
2024-12-25 20:25:12 +0300

    Add a footer

--- frontend/src/Layouts/MainLayout.tsx
+++ frontend/src/Layouts/MainLayout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <main className="flex h-auto w-full items-center justify-center overflow-y-scroll bg-sky-950 px-14 pb-24 pt-24">
-      <Outlet />
-    </main>
+    <>
+      <main className="flex h-auto w-full flex-col items-center justify-center bg-sky-950 px-14 pb-24 pt-24">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 };
 
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex h-fit w-full items-center justify-center bg-sky-900 py-12 text-lg text-slate-400">
+      Made with <span className="ml-2 text-xl text-sky-900">❤️</span>
+    </div>
+  );
+};
+
+export default Footer;
