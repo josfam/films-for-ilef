cfd0e12473e4927d138c7ae96b41434dc89085ef
josfam <jamukun@proton.me>
2024-12-25 22:27:49 +0300

    Add snow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FilmGrid from "./pages/FilmGrid";
 import MainLayout from "./Layouts/MainLayout";
 import { Toaster } from "sonner";
+import Snowfall from "react-snowfall";
 
 function App() {
   return (
     <>
+      <Snowfall
+        snowflakeCount={15}
+        speed={[1, 1]}
+        wind={[1, 2]}
+        radius={[1, 2]}
+        style={{
+          position: "fixed",
+          zIndex: 20,
+          height: "100svh",
+          width: "100%",
+        }}
+      />
       {/* Toast messages */}
       <Toaster richColors position="top-center" visibleToasts={4} />
       <Router>
