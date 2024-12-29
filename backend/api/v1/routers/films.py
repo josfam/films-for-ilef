5a90127b6c55adc0032fc0cae684828f157f3b8d
josfam <jamukun@proton.me>
2024-12-29 13:02:41 +0300

    Add debug lines to test deployment locations

--- backend/api/v1/routers/films.py
+++ backend/api/v1/routers/films.py
@@ -13,10 +13,11 @@ def get_recommendations(response: Response):
     """Returns films in the film list file that have not been watched yet"""
     films_obj = None
     film_file = str(Path.cwd() / 'api' / 'v1' / 'storage' / 'films.json')
-
+    print('Film file location', film_file)  # DEBUG
     try:
         with open(film_file, 'r', encoding='utf-8') as f:
             films_obj = json.load(f)
+            print('Films object', films_obj)  # DEBUG
     except Exception as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {"message": "Can't load the films right now. Try again"}
