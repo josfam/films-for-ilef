68382a02fcab372b55fd529ddce788dfa398b6b5
josfam <jamukun@proton.me>
2024-12-29 09:26:10 +0300

    Remove unused FilmCardSkeleton imports

--- frontend/src/components/FilmCard.tsx
+++ frontend/src/components/FilmCard.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { SuccessToast } from "./toasts/Toasts";
-import { FilmCardSkeleton } from "./LoadingSkeletons";
 
 interface FilmCardProps {
   coverName: string;
